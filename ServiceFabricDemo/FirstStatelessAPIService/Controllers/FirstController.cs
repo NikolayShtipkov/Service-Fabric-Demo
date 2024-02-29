01ae76b42aec210b8fab4c29c2af9a9a14266324
unknown <nikolayshtipkov@gmail.com>
2024-02-29 09:55:56 +0200

    add first stateless api in service fabric

---
+++ ServiceFabricDemo/FirstStatelessAPIService/Controllers/FirstController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstStatelessAPIService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FirstController : ControllerBase
+    {
+        [HttpGet]
+        [Route("get")]
+        public async Task Get()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
